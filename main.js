c57cc7304c5be743ed2fe7fb43fbb615554da153
willr-webdev <81498360+willr-webdev@users.noreply.github.com>
2021-10-16 01:14:52 -0400

    u

--- main.js
+++ main.js
@@ -8,12 +8,27 @@
 //   document.querySelector(".fa-bars").style.display = "flex";
 // }
 
-function dimMain() {
-  var checkbox = document.getElementById('recipes_checkbox');
+// function dimMain() {
+//   var checkbox = document.getElementById('recipes_checkbox');
+
+//   if (checkbox.checked) {
+//     document.querySelector(".main-wrapper").style.opacity = "0";
+//   } else {
+//     document.querySelector(".main-wrapper").style.opacity = "1";
+//   }
+// }
+
+function colorBlogGroup() {
+  var checkbox = document.getElementById("recipes_checkbox");
 
   if (checkbox.checked) {
-    document.querySelector(".main-wrapper").style.opacity = "0";
-  } else {
-    document.querySelector(".main-wrapper").style.opacity = "1";
+    document.querySelector("div.cat-2").style.backgroundColor = "var(--df-main)";
+    document.querySelector(".recipes_toggle").style.fontWeight = "bold";
+    document.querySelector(".recipes_toggle").style.fontStyle = "italic";
+  }
+  else {
+    document.querySelector("div.cat-2").style.backgroundColor = "rgba(255, 255, 255, .65)";
+    document.querySelector(".recipes_toggle").style.fontWeight = "normal";
+    document.querySelector(".recipes_toggle").style.fontStyle = "normal";
   }
 }
