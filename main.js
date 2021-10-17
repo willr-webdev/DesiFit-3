d3ba4dc64e66841f8c4ae242728becf3935cb28e
willr-webdev <81498360+willr-webdev@users.noreply.github.com>
2021-10-17 00:33:35 -0400

    u

--- main.js
+++ main.js
@@ -18,7 +18,22 @@
 //   }
 // }
 
-function colorBlogGroup() {
+function colorAllGroup() {
+  var checkbox = document.getElementById("all_checkbox");
+
+  if (checkbox.checked) {
+    document.querySelector("div.cat-1").style.backgroundColor = "var(--df-main)";
+    document.querySelector(".all_toggle").style.fontWeight = "bold";
+    document.querySelector(".all_toggle").style.fontStyle = "italic";
+  }
+  else {
+    document.querySelector("div.cat-1").style.backgroundColor = "rgba(255, 255, 255, .7)";
+    document.querySelector(".all_toggle").style.fontWeight = "normal";
+    document.querySelector(".all_toggle").style.fontStyle = "normal";
+  }
+}
+
+function colorRecipesGroup() {
   var checkbox = document.getElementById("recipes_checkbox");
 
   if (checkbox.checked) {
@@ -27,8 +42,68 @@ function colorBlogGroup() {
     document.querySelector(".recipes_toggle").style.fontStyle = "italic";
   }
   else {
-    document.querySelector("div.cat-2").style.backgroundColor = "rgba(255, 255, 255, .65)";
+    document.querySelector("div.cat-2").style.backgroundColor = "rgba(255, 255, 255, .25)";
     document.querySelector(".recipes_toggle").style.fontWeight = "normal";
     document.querySelector(".recipes_toggle").style.fontStyle = "normal";
+  }
+}
+
+function colorFitnessGroup() {
+  var checkbox = document.getElementById("fitness_checkbox");
+
+  if (checkbox.checked) {
+    document.querySelector("div.cat-3").style.backgroundColor = "var(--df-main)";
+    document.querySelector(".fitness_toggle").style.fontWeight = "bold";
+    document.querySelector(".fitness_toggle").style.fontStyle = "italic";
+  }
+  else {
+    document.querySelector("div.cat-3").style.backgroundColor = "rgba(255, 255, 255, .7)";
+    document.querySelector(".fitness_toggle").style.fontWeight = "normal";
+    document.querySelector(".fitness_toggle").style.fontStyle = "normal";
+  }
+}
+
+function colorNutritionGroup() {
+  var checkbox = document.getElementById("nutrition_checkbox");
+
+  if (checkbox.checked) {
+    document.querySelector("div.cat-4").style.backgroundColor = "var(--df-main)";
+    document.querySelector(".nutrition_toggle").style.fontWeight = "bold";
+    document.querySelector(".nutrition_toggle").style.fontStyle = "italic";
+  }
+  else {
+    document.querySelector("div.cat-4").style.backgroundColor = "rgba(255, 255, 255, .25)";
+    document.querySelector(".nutrition_toggle").style.fontWeight = "normal";
+    document.querySelector(".nutrition_toggle").style.fontStyle = "normal";
+  }
+}
+
+function colorMotivationGroup() {
+  var checkbox = document.getElementById("motivation_checkbox");
+
+  if (checkbox.checked) {
+    document.querySelector("div.cat-5").style.backgroundColor = "var(--df-main)";
+    document.querySelector(".motivation_toggle").style.fontWeight = "bold";
+    document.querySelector(".motivation_toggle").style.fontStyle = "italic";
+  }
+  else {
+    document.querySelector("div.cat-5").style.backgroundColor = "rgba(255, 255, 255, .7)";
+    document.querySelector(".motivation_toggle").style.fontWeight = "normal";
+    document.querySelector(".motivation_toggle").style.fontStyle = "normal";
+  }
+}
+
+function colorOtherGroup() {
+  var checkbox = document.getElementById("other_checkbox");
+
+  if (checkbox.checked) {
+    document.querySelector("div.cat-6").style.backgroundColor = "var(--df-main)";
+    document.querySelector(".other_toggle").style.fontWeight = "bold";
+    document.querySelector(".other_toggle").style.fontStyle = "italic";
+  }
+  else {
+    document.querySelector("div.cat-6").style.backgroundColor = "rgba(255, 255, 255, .25)";
+    document.querySelector(".other_toggle").style.fontWeight = "normal";
+    document.querySelector(".other_toggle").style.fontStyle = "normal";
   }
 }
